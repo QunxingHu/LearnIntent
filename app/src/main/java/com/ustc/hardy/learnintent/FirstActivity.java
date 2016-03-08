b9529eea9247fbedbb3ee788c25353245253a906
Qunxing_hu <1367811357@qq,cin>
2016-03-08 21:56:23 +0800

    Second commit

--- app/src/main/java/com/ustc/hardy/learnintent/FirstActivity.java
+++ app/src/main/java/com/ustc/hardy/learnintent/FirstActivity.java
@@ -2,12 +2,24 @@ package com.ustc.hardy.learnintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
