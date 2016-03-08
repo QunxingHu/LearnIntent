deb2fb1b4ef65e5d0e73bb08363a8acac2981742
Qunxing_hu <1367811357@qq,cin>
2016-03-08 21:52:35 +0800

    Initial commit

---
+++ app/src/main/java/com/ustc/hardy/learnintent/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.ustc.hardy.learnintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhihu on 2016/3/8.
+ */
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
